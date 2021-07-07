133101d157847e3e4afdd11b8fa800001eeee3eb
Andrew Kilduff <a.l.kilduff@gmail.com>
2021-07-07 12:04:57 -0700

    Update to three assertion tests

--- index.js
+++ index.js
@@ -16,53 +16,76 @@ async function runTestWithCaps (capabilities) {
     await inputField.submit(); // this helps in mobile browsers
   }
 
-  // Check to make sure title page contains BrowserStack. No assertions
+  // First Assertion - Page Title Equality
   try {
     await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
-    console.log(await driver.getTitle());
-    await driver.executeScript(
-      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title contains BrowserStack!"}}'
-    );
+    const actualTitle = await driver.getTitle();
+    if (await assert.equal(actualTitle, 'BrowserStack - Google Search') === true) {
+      await driver.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title is exactly BrowserStack - Google Search"}}'
+      );
+    } else {
+      await driver.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Title is NOT exactly BrowserStack - Google Search"}}'
+      );
+    }
   } catch (e) {
+    const actualTitle = await driver.getTitle();
     await driver.executeScript(
       'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Page could not load in time"}}'
     );
   }
 
-  // First Assertion
+  // Second Assertion - Test Link Text Equality
   try {
     await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
-    const actualTitle = await driver.getTitle();
-    await assert.equal(actualTitle, 'BrowserStack - Google Search')
-    await driver.executeScript(
-      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title is exactly BrowserStack - Google Search"}}'
-    );
+    const testLinkDesktop = await driver.findElement(webdriver.By.xpath(`//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/a/h3`));
+    const linkTextDesktop = await testLinkDesktop.getText(); // What is the text of the Desktop link
+    if (await assert.equal(linkTextDesktop, 'BrowserStack: Most Reliable App & Cross Browser Testing Platform...') === true) {
+      await driver.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Desktop Link Text matches expected Google Results for BrowserStack"}}'
+      );
+    } else {
+      await driver.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Desktop Link Text does NOT match expected Google Results for BrowserStack"}}'
+      );
+    }
   } catch (e) {
-    await driver.executeScript(
-      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Title is NOT exactly BrowserStack - Google Search"}}'
-    );
+    try {
+      await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
+      const testLinkMobile = await driver.findElement(webdriver.By.xpath(`//*[@id="rso"]/div[1]/div/div[1]/div/div[1]/a/div[2]/div`));
+      const linkTextMobile = await testLinkMobile.getText(); // What is the text of the Mobile link
+      if (await assert.equal(testLinkMobile, 'BrowserStack') === true)
+      await driver.executeScript(
+      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Mobile Link Text matches expected Google Results for BrowserStack"}}'
+      );
+    } catch (e) {
+      await driver.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Link Text does NOT match expected Google Results for BrowserStack"}}'
+      );
+    }
   }
 
-  // Second Test
+  // Third Assertion - Navigation to First Link on Desktop & Page Title Equality
   try {
     await driver.wait(webdriver.until.titleMatches(/BrowserStack/i), 5000);
     const testLink = await driver.findElement(webdriver.By.xpath(`//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/a/h3`));
     await testLink.click(); // Click on the link
-    try {
-      await driver.wait(webdriver.until.titleMatches(/Most/i), 5000);
-      const linkTitle = await driver.getTitle();
-      await assert.equal(linkTitle, 'Most Reliable App & Cross Browser Testing Platform | BrowserStack')
-      await driver.executeScript(
-        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Link Title matches BrowserStack homepage"}}'
-      );
-    } catch (e) {
-      await driver.executeScript(
-        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Link did not load in time"}}'
-      );
-    }
+    await driver.wait(webdriver.until.titleMatches(/Most/i), 5000);
+    const linkTitle = await driver.getTitle();
+    console.log(linkTitle)
+      if (await assert.equal(linkTitle, `Most Reliable App & Cross Browser Testing Platform | BrowserStack`) === true) {
+        await driver.executeScript(
+          'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Link Title matches BrowserStack homepage"}}'
+        );
+      } else {
+        await driver.executeScript(
+          'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Link Title does NOT match BrowserStack homepage"}}'
+        );
+      }
   } catch (e) {
     await driver.executeScript(
-      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Link  cannot be found and clicked"}}'
+      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "No link to click"}}'
     );
   }
 
